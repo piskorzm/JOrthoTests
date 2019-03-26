a2134f0fd998edfa5f3d53974190d4857e333d7a
Marcin Piskorz <marcinpiskorz01@gmail.com>
2019-03-26 18:50:42 +0100

    JOrtho setup.

---
+++ tests/AllTests.java
@@ -0,0 +1,10 @@
+import junit.framework.Test;
+import junit.framework.TestSuite;
+
+public class AllTests {
+
+    public static Test suite() {
+        TestSuite suite = new TestSuite( "JOrtho Tests" );
+        return suite;
+    }
+}
